e1c480d9140204b192efcdd8133650ba223b985f
Phuong <phuong.tran@jvncomm.com>
2020-02-07 12:19:44 -0500

    Remove unneeded functions in base class.

--- Ditto_ActiveMQ/Topic.cpp
+++ Ditto_ActiveMQ/Topic.cpp
@@ -4,17 +4,12 @@ size_t Topic::get_not_found_list_size() {
 	return not_found_list_.size();
 }
 
-void Topic::reset_builder() {
-	flexbuffers_builder_.Clear();
-}
-
 // Remove all the dataref in the dataref list
 void Topic::empty_list() {
 	// Try and get access to dataref_list_
 	std::scoped_lock guard(data_lock);
 	dataref_list_.clear();
 	not_found_list_.clear();
-	reset_builder();
 }
 
 Topic::Topic(TopicRole role, const std::string& topic, const std::string& address, const std::string& config) :
@@ -25,82 +20,6 @@ Topic::Topic(TopicRole role, const std::string& topic, const std::string& addres
 	dataref_list_{},
 	not_found_list_{}
 {
-}
-
-const std::vector<uint8_t>& Topic::get_flexbuffers_data() {
-	// Try and get access to dataref_list_
-	std::scoped_lock guard(data_lock);
-
-	const auto map_start = flexbuffers_builder_.StartMap();
-
-	for (const auto& dataref : dataref_list_) {
-		switch (dataref.type) {
-		case DatarefType::INT: {
-			if (dataref.start_index.has_value()) {
-				// If start index exist then it's an array
-				auto int_num = get_value<std::vector<int>>(dataref);
-				if (2 <= dataref.num_value.value() && dataref.num_value.value() <= 4) {
-					flexbuffers_builder_.FixedTypedVector(dataref.name.c_str(),
-						int_num.data(),
-						dataref.num_value.value());
-				}
-				else {
-					flexbuffers_builder_.TypedVector(dataref.name.c_str(), [&] {
-						for (auto i : int_num) {
-							flexbuffers_builder_.Int(i);
-						}
-						});
-				}
-			}
-			else {
-				// Just single value
-				auto return_value = get_value<int>(dataref);
-				flexbuffers_builder_.Int(dataref.name.c_str(), return_value);
-			}
-			break;
-		}
-		case DatarefType::FLOAT: {
-			if (dataref.start_index.has_value()) {
-				auto float_num = get_value<std::vector<float>>(dataref);
-				if (2 <= dataref.num_value.value() && dataref.num_value.value() <= 4) {
-					flexbuffers_builder_.FixedTypedVector(dataref.name.c_str(),
-						float_num.data(),
-						dataref.num_value.value());
-				}
-				else {
-					flexbuffers_builder_.TypedVector(dataref.name.c_str(), [&] {
-						for (auto i : float_num) {
-							flexbuffers_builder_.Float(i);
-						}
-						});
-				}
-			}
-			else {
-				flexbuffers_builder_.Float(dataref.name.c_str(), get_value<float>(dataref));
-			}
-			break;
-		}
-		case DatarefType::DOUBLE: {
-			flexbuffers_builder_.Double(dataref.name.c_str(), get_value<double>(dataref));
-			break;
-		}
-		case DatarefType::STRING: {
-			flexbuffers_builder_.String(dataref.name.c_str(), get_value<std::string>(dataref).c_str());
-			break;
-		}
-		default:
-			break;
-		}
-	}
-
-	flexbuffers_builder_.EndMap(map_start);
-	flexbuffers_builder_.Finish();
-
-	return flexbuffers_builder_.GetBuffer();
-}
-
-size_t Topic::get_flexbuffers_size() {
-	return flexbuffers_builder_.GetSize();
 }
 
 void Topic::set_retry_limit() {
@@ -224,9 +143,6 @@ bool Topic::init()
 
 void Topic::update()
 {
-	const auto out_data = get_flexbuffers_data();
-	const auto size = get_flexbuffers_size();
-	reset_builder();
 }
 
 void Topic::shutdown()
--- Ditto_ActiveMQ/Topic.h
+++ Ditto_ActiveMQ/Topic.h
@@ -49,85 +49,14 @@ private:
 private:
 	bool get_data_list();
 	void set_retry_limit();
-	const std::vector<uint8_t>& get_flexbuffers_data();
-	size_t get_flexbuffers_size();
 	size_t get_not_found_list_size();
 	void retry_dataref();
 	void empty_list();
-	void reset_builder();
-
-	template<typename T, std::enable_if_t<std::is_same_v<T, int>, int> = 0>
-	decltype(auto) get_value(const DatarefInfo& in_dataref) {
-		return XPLMGetDatai(in_dataref.dataref);
-	}
-
-	template<typename T, std::enable_if_t<std::is_same_v<T, float>, int> = 0>
-	decltype(auto) get_value(const DatarefInfo& in_dataref) {
-		return XPLMGetDataf(in_dataref.dataref);
-	}
-
-	template<typename T, std::enable_if_t<std::is_same_v<T, double>, int> = 0>
-	decltype(auto) get_value(const DatarefInfo& in_dataref) {
-		return XPLMGetDatad(in_dataref.dataref);
-	}
-
-	template<typename T, std::enable_if_t<std::is_same_v<T, std::vector<int>>, int> = 0>
-	decltype(auto) get_value(const DatarefInfo& in_dataref) {
-		std::vector<int> temp{};
-		temp.resize(in_dataref.num_value.value());
-		XPLMGetDatavi(in_dataref.dataref, temp.data(), in_dataref.start_index.value(), in_dataref.num_value.value());
-		return temp;
-	}
-
-	template<typename T, std::enable_if_t<std::is_same_v<T, std::vector<float>>, int> = 0>
-	decltype(auto) get_value(const DatarefInfo& in_dataref) {
-		std::vector<float> temp{};
-		temp.resize(in_dataref.num_value.value());
-		XPLMGetDatavf(in_dataref.dataref, temp.data(), in_dataref.start_index.value(), in_dataref.num_value.value());
-		return temp;
-	}
-
-	template<typename T, std::enable_if_t<std::is_same_v<T, std::string>, int> = 0>
-	decltype(auto) get_value(const DatarefInfo& in_dataref) {
-		// Get the current string size only first
-		auto current_string_size = XPLMGetDatab(in_dataref.dataref, nullptr, 0, 0);
-
-		// Only get data when there is something in the string dataref
-		if (current_string_size != 0) {
-			if (!in_dataref.start_index.has_value()) {
-				// Get the whole string
-				auto temp_buffer_size = current_string_size + 1;
-				auto temp = std::make_unique<char[]>(temp_buffer_size);
-				XPLMGetDatab(in_dataref.dataref, temp.get(), 0, current_string_size);
-				return std::string(temp.get());
-			}
-			else {
-				if (!in_dataref.num_value.has_value()) {
-					// Get the string from start_index to the end
-					auto temp_buffer_size = current_string_size + 1;
-					auto temp = std::make_unique<char[]>(temp_buffer_size);
-					XPLMGetDatab(in_dataref.dataref, temp.get(), in_dataref.start_index.value(), current_string_size);
-					return std::string(temp.get());
-				}
-				else {
-					// Get part of the string starting from start_index until
-					// number_of_value is reached
-					auto temp_buffer_size = in_dataref.num_value.value() + 1;
-					if (in_dataref.num_value.value() <= current_string_size) {
-						auto temp = std::make_unique<char[]>(temp_buffer_size);
-						XPLMGetDatab(in_dataref.dataref, temp.get(), in_dataref.start_index.value(), in_dataref.num_value.value());
-						return std::string(temp.get());
-					}
-				}
-			}
-		}
-		return std::string();
-	}
 
 public:
 	explicit Topic(TopicRole role, const std::string& topic, const std::string& address, const std::string& config);
-	bool init();
-	void update();
-	void shutdown();
+	virtual ~Topic() = default;
+	virtual bool init();
+	virtual void update();
+	virtual void shutdown();
 };
-
