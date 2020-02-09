538cf95f1c75e5f53c2731b7051026ecc27933bf
phuongtran7 <phuong.tran@jvncomm.com>
2020-02-09 18:56:31 -0500

    Add MQTT publisher

--- Ditto_ActiveMQ/MQTT.cpp
+++ Ditto_ActiveMQ/MQTT.cpp
@@ -1,1 +1,56 @@
 #include "MQTT.h"
+
+MQTT_Publisher::MQTT_Publisher(const std::string& address, const std::string& topic, int qos) :
+	address_(address),
+	topic_(topic),
+	qos_(qos),
+	client_(address_, ""), // Force random clientID
+	conn_options_{}
+{
+	conn_options_.set_clean_session(true);
+	client_.set_callback(*this);
+	client_.connect(conn_options_)->wait();
+}
+
+MQTT_Publisher::~MQTT_Publisher()
+{
+	client_.disconnect()->wait();
+}
+
+void MQTT_Publisher::send_message(const std::string& message)
+{
+	mqtt::message_ptr pubmsg = mqtt::make_message(topic_, message.c_str(), message.size(), 0, false);
+	client_.publish(pubmsg, nullptr, *this);
+}
+
+void MQTT_Publisher::send_message(const std::vector<uint8_t>& pointer, size_t size)
+{
+	mqtt::message_ptr pubmsg = mqtt::make_message(topic_, pointer.data(), size, 0, false);
+	client_.publish(pubmsg, nullptr, *this);
+}
+
+void MQTT_Publisher::delivery_complete(mqtt::delivery_token_ptr token)
+{
+	fmt::print("Delivered message.\n");
+}
+
+void MQTT_Publisher::on_failure(const mqtt::token& tok)
+{
+	fmt::print("failed.\n");
+}
+
+void MQTT_Publisher::on_success(const mqtt::token& tok)
+{
+	fmt::print("Succeed.\n");
+}
+
+void MQTT_Publisher::connected(const std::string& cause)
+{
+	fmt::print("Connection success. Will publish to {}.\n", topic_);
+}
+
+void MQTT_Publisher::connection_lost(const std::string& cause)
+{
+	fmt::print("Connection lost: {}.\nReconnecting...\n", cause);
+	client_.reconnect()->wait();
+}
--- Ditto_ActiveMQ/MQTT.h
+++ Ditto_ActiveMQ/MQTT.h
@@ -1,5 +1,29 @@
 #pragma once
-class MQTT
-{
-};
+#include "mqtt/async_client.h"
+#include <mqtt/callback.h>
+#include "fmt/format.h"
 
+class MQTT_Publisher : public virtual mqtt::callback,
+	public virtual mqtt::iaction_listener
+{
+public:
+	MQTT_Publisher(const std::string& address, const std::string& topic, int qos);
+	~MQTT_Publisher();
+
+	void send_message(const std::string& message);
+	void send_message(const std::vector<uint8_t>& pointer, size_t size);
+
+private:
+	std::string address_;
+	std::string topic_;
+	int qos_;
+	mqtt::async_client client_;
+	mqtt::connect_options conn_options_;
+
+private:
+	void delivery_complete(mqtt::delivery_token_ptr token) override;
+	void on_failure(const mqtt::token& tok) override;
+	void on_success(const mqtt::token& tok) override;
+	void connected(const std::string& cause) override;
+	void connection_lost(const std::string& cause) override;
+};
