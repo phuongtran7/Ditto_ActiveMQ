3f33725eba4b89e5112bf0a46d7c652e2f788461
phuongtran7 <phuong.tran@jvncomm.com>
2020-02-09 19:07:05 -0500

    Add MQTT subscriber class.

--- Ditto_ActiveMQ/MQTT.cpp
+++ Ditto_ActiveMQ/MQTT.cpp
@@ -7,14 +7,24 @@ MQTT_Publisher::MQTT_Publisher(const std::string& address, const std::string& to
 	client_(address_, ""), // Force random clientID
 	conn_options_{}
 {
-	conn_options_.set_clean_session(true);
-	client_.set_callback(*this);
-	client_.connect(conn_options_)->wait();
+	try {
+		conn_options_.set_clean_session(true);
+		client_.set_callback(*this);
+		client_.connect(conn_options_)->wait();
+	}
+	catch (const mqtt::exception & exc) {
+		fmt::print(exc.what());
+	}
 }
 
 MQTT_Publisher::~MQTT_Publisher()
 {
-	client_.disconnect()->wait();
+	try {
+		client_.disconnect()->wait();
+	}
+	catch (const mqtt::exception & exc) {
+		fmt::print(exc.what());
+	}
 }
 
 void MQTT_Publisher::send_message(const std::string& message)
@@ -50,6 +60,67 @@ void MQTT_Publisher::connected(const std::string& cause)
 }
 
 void MQTT_Publisher::connection_lost(const std::string& cause)
+{
+	fmt::print("Connection lost: {}.\nReconnecting...\n", cause);
+	client_.reconnect()->wait();
+}
+
+MQTT_Subscriber::MQTT_Subscriber(const std::string& address, const std::string& topic, int qos) :
+	address_(address),
+	topic_(topic),
+	qos_(qos),
+	client_(address_, ""), // Force random clientID
+	conn_options_{}
+{
+	try {
+		conn_options_.set_keep_alive_interval(20);
+		conn_options_.set_clean_session(true);
+		client_.set_callback(*this);
+		client_.connect(conn_options_)->wait();
+	}
+	catch (const mqtt::exception & exc) {
+		fmt::print(exc.what());
+	}
+}
+
+MQTT_Subscriber::~MQTT_Subscriber()
+{
+	try {
+		client_.unsubscribe(topic_)->wait();
+		client_.stop_consuming();
+		client_.disconnect()->wait();
+	}
+	catch (const mqtt::exception & exc) {
+		fmt::print(exc.what());
+	}
+}
+
+void MQTT_Subscriber::message_arrived(mqtt::const_message_ptr msg)
+{
+	//fmt::print("Topic: {} - Payload: {}\n", msg->get_topic(), msg->to_string());
+	auto payload = msg->get_payload();
+	auto recieved = flexbuffers::GetRoot(reinterpret_cast<const uint8_t*>(payload.data()), payload.size()).AsFloat();
+	fmt::print("Got: {}\n", recieved);
+}
+
+void MQTT_Subscriber::on_failure(const mqtt::token& tok)
+{
+	fmt::print("Connection attempt failed. Will try to reconnect.\n");
+	client_.reconnect()->wait();
+}
+
+void MQTT_Subscriber::on_success(const mqtt::token& tok)
+{
+	fmt::print("Succeed.\n");
+}
+
+void MQTT_Subscriber::connected(const std::string& cause)
+{
+	fmt::print("Connection success. Will subscribte to {}.\n", topic_);
+	client_.subscribe(topic_, qos_, nullptr, *this);
+}
+
+void MQTT_Subscriber::connection_lost(const std::string& cause)
 {
 	fmt::print("Connection lost: {}.\nReconnecting...\n", cause);
 	client_.reconnect()->wait();
